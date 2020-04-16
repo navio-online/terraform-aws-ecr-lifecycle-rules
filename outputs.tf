939eef61f029702480303dc2ee7296d55643e04b
Peter Salnikov <peter@salnikov.cc>
2020-04-17 00:55:27 +0300

    module for ecr lifecycle rules - initial commit

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+# If no priority is set, the default value for priority priority:replace:this will be replaced with $${rule_priority} for later 
+# template interpolation
+output "policy_rule" {
+  description = "ECR Policy Rule"
+
+  value = "${replace(
+		 replace(element(concat(
+             data.template_file.lifecycle_policy_imageCountMoreThan_tagged.*.rendered,
+             data.template_file.lifecycle_policy_imageCountMoreThan_untagged_or_any.*.rendered,
+             data.template_file.lifecycle_policy_sinceImagePushed_tagged.*.rendered,
+             data.template_file.lifecycle_policy_sinceImagePushed_untagged_or_any.*.rendered,
+             list("")
+			      ),0),
+			 "/\"(true|false|[[:digit:]]+)\"/", "$1"
+		), "string:", ""
+	      )}"
+}
